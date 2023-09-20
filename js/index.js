6063975c88c7609d4efcb092f713497d8990a7d7
albadr <kaitokitthebest@gmail.com>
2023-09-21 02:47:15 +0300

    Delete unused file & adjust files names and pathes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
-
-function openNav () {
-    document.getElementById ("snd").style.width = "250px";
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
 }
-function closeNav () {
-    document.getElementById ("snd").style.width = "0";
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
 }
- // this function change background color
- function changeColor(black) { document.body.style.backgroundColor = black; }
- //alert('press OK If u are dumb')
- /// This is a Dumb man
- console.log('if u read this congrats u are still dumb')
+// this function change background color
+function changeColor(black) {
+    document.body.style.backgroundColor = black;
+}
+//alert('press OK If u are dumb')
+/// This is a Dumb man
+console.log("if u read this congrats u are still dumb");
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Editing Feras_MO site
+
+### here are the done changes (commits):
+
+-   Delete unused files and adjust files names and pathes.
+-

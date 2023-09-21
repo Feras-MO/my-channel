353977afecc8fcbd59cfdc42481ac9df2eac4f79
albadr <kaitokitthebest@gmail.com>
2023-09-21 05:07:13 +0300

    U r the dumb one here

--- js/index.js
+++ js/index.js
@@ -4,10 +4,7 @@ function openNav() {
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
 }
-// this function change background color
 function changeColor(black) {
     document.body.style.backgroundColor = black;
 }
-//alert('press OK If u are dumb')
-/// This is a Dumb man
-console.log("if u read this congrats u are still dumb");
+console.log("feras is the dumbest");
